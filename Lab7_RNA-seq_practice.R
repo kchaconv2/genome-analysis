d1531e291d1b6f86a24707591face14dd3573ac7
KChacon-Vargas <kchaconvarga@umass.edu>
2020-03-26 18:20:58 -0400

    updated labs 7, 8 , 9

---
+++ Lab7_RNA-seq_practice.R
@@ -0,0 +1,15 @@
+if (!requireNamespace("BiocManager", quietly = TRUE))
+  install.packages("BiocManager")
+
+BiocManager::install("rnaseqGene")
+BiocManager::install("DESeq2")
+BiocManager::install("airway")
+BiocManager::install("tximeta")
+
+BiocManager::install("Gviz")
+BiocManager::install("sva")
+BiocManager::install("RUVSeq")
+BiocManager::install("fission")
+
+
+
