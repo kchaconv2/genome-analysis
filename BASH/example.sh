4e6191d30312928fd0fd9e433b6c262f80499069
KChacon-Vargas <kchaconvarga@umass.edu>
2020-04-27 19:13:00 -0400

    labs

---
+++ BASH/example.sh
@@ -0,0 +1,6 @@
+pwd #path to workDir
+pwd > class_path.txt # copy path in new file
+more my_info | grep Armenia #view my_info file and grep Armenia
+grep 'Katherine' my_info > class_path.txt #grep Katherine anc copy it to class_path.txt file
+cp class_path.txt class_name.txt # copy class
+rm class_path.txt # remove class_path.txt file
---
+++ Lab9_Covid19_maps_info.R
@@ -0,0 +1,10 @@
+https://www.r-bloggers.com/map-visualization-of-covid-19-across-the-world-with-r/
+  https://www.r-spatial.org/r/2018/10/25/ggplot2-sf.html
+https://moodle.umass.edu/pluginfile.php/3107900/mod_resource/content/2/undefined/Lab9_HuGen.html
+https://github.com/CSSEGISandData/COVID-19/tree/master/csse_covid_19_data
+https://richardlent.github.io/rnotebooks/maps.nb.html
+https://www.datanovia.com/en/blog/how-to-create-a-map-using-ggplot2/
+  https://stackoverflow.com/questions/42285203/unable-to-change-color-in-r-in-a-usa-map
+http://search.r-project.org/library/animint2/html/borders.html
+https://suzan.rbind.io/2018/02/dplyr-tutorial-3/
+  
